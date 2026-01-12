5681fe2ea1ac35c3cc06df5995af5a7814429343
Damien Levrault <damien.levrault@etu.u-bordeaux.fr>
2026-01-12 16:46:44 +0100

    Foo class implementation

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Foo {
+
+    private Bar bar;
+    private ArrayList<Baz> bazs = new ArrayList<Baz>();
+    private Qux qux;
+    private Corge corge;
+    private ArrayList<Grault> graults = new ArrayList<>();
+    Foo(Bar bar) {
+        this.bar = bar;
+        this.qux = new Qux();
+        this.corge = new Corge();
+    }
+
+    public void addBaz(Baz baz) {
+        if (baz == null) {
+            return;
+        }
+        this.bazs.add(baz);
+    }
+
+    public void addGrault() {
+        return;
+    }
 }
